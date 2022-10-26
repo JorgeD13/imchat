ddebc5c03493be2b59af213b19fdd580111eb6f2
Renato <luis.carbajal@utec.edu.pe>
2022-10-26 11:49:17 -0500

    backend project

---
+++ imchat-backend/src/main/java/com/seguridad/imchatbackend/ImchatBackendApplication.java
@@ -0,0 +1,13 @@
+package com.seguridad.imchatbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImchatBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImchatBackendApplication.class, args);
+	}
+
+}
---
+++ imchat-backend/src/test/java/com/seguridad/imchatbackend/ImchatBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seguridad.imchatbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImchatBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
